6b1c33d9bb52d5e92e5dc7674f6a5c0ccf7b3b38
sangeeth Satyada <sangeethraju.satyada@virtukart.com>
2025-11-09 19:15:23 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
 
 const connectDb = async()=>{
-  try{
-    mongoose.connect(process.env.ADMIN_DB_URL).then(()=>{
+
+   await  mongoose.connect(process.env.ADMIN_DB_URL).then(()=>{
       console.log("Connected to DB")
     }).catch((err)=>{
       console.log(err)
     })
-  }catch(err){
-    console.log(err)
-  }
+
 }
 module.exports = connectDb;
